9ec234739dbdd6ed58da646644348cec6262e586
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-14 14:08:15 +0530

    Update reporting_sql.py

--- Harita/modules/sql/reporting_sql.py
+++ Harita/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from Evie.modules.sql import BASE, SESSION
+from Harita.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
