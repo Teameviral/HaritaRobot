c12c8d6e33122cbcb41200a36ea65b45f6afd1ea
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-14 14:11:35 +0530

    Update warns_sql.py

--- Harita/modules/sql/warns_sql.py
+++ Harita/modules/sql/warns_sql.py
@@ -1,7 +1,7 @@
 import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct
 
-from Evie.modules.sql import SESSION, BASE
+from Harita.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
