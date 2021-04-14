8849e4f93422a072ef2e91aba491fd40209495a7
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-14 14:09:57 +0530

    Update urlblacklist_sql.py

--- Harita/modules/sql/urlblacklist_sql.py
+++ Harita/modules/sql/urlblacklist_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String, UnicodeText
-from Evie.modules.sql import BASE, SESSION
+from Harita.modules.sql import BASE, SESSION
 
 
 class URLBlackListFilters(BASE):
