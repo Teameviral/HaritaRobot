962f75a3f470ccbd7d74a6d709daa99e8a8e31df
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-14 14:10:20 +0530

    Update voice_chat_sql.py

--- Harita/modules/sql/voice_chat_sql.py
+++ Harita/modules/sql/voice_chat_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from Evie.modules.sql import BASE, SESSION
+from Harita.modules.sql import BASE, SESSION
 
 
 class Talkmode(BASE):
