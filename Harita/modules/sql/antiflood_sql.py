50b0b3f5ea6d8e84acfdfda80283dc0342d594d5
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-14 14:02:35 +0530

    Update antiflood_sql.py

--- Harita/modules/sql/antiflood_sql.py
+++ Harita/modules/sql/antiflood_sql.py
@@ -1,7 +1,7 @@
 import threading
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from Evie.modules.sql import SESSION, BASE
+from Harita.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
